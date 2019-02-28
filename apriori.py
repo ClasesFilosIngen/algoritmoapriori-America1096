4c721b4e0226ecd43f37711762fed6d11df6e21c
America1096 <33714075+America1096@users.noreply.github.com>
2019-02-28 10:37:10 -0600

    Actualizacion para trabajar con conjuntos

--- apriori.py
+++ apriori.py
@@ -1,59 +1,61 @@
+##Función que permite crear C1, obteniendo una lista de conjuntos unitarios
 def init_pass(T):
     C1 = []
-    for x in range(len(T)):
-    	for y in range(len(T[x])):
-    		if not T[x][y] in C1:
-    			C1.append(T[x][y])
+    for conjunto in T:
+        for elemento in conjunto:
+            if not [elemento] in C1:
+                C1.append([elemento])
+                
     C1.sort()
-    return C1
+    return list(map(frozenset, C1))#Permite crear conjuntos
 
-def obtenerF(T, C, minsup):
-	dic = {}
-	for conjunto in T:
-		for elemento in conjunto:
-			dic.setdefault(elemento, 0)
-			keys = dic.keys()
-			if elemento in keys:
-				dic.update({elemento : dic.get(elemento)+1})
-	
-	for key in dic:
-		if dic[key]/len(C) < minsup:
-			C.remove(key)			
-	return C
+##Permite crear el conjunto F, que da como resultado los conjuntos de elementos frecuentes
+##Para que un conjuntos sea frecuente sus subconjuntos deben ser frecuentes 
+def obtenerF(T, Ck, minSup):
+    repeticiones = {}
+    for conjunto in T:
+        for elemento in Ck:
+            if elemento.issubset(conjunto):
+                if not elemento in repeticiones:
+                    repeticiones.setdefault(elemento, 1)
+                else:
+                    repeticiones.update({elemento : repeticiones.get(elemento)+1})
 
-def cand_gen(F, T):
-	C, aux = [], []
-	i = 0
-	for f1 in range(len(F)-1):
-		for f2 in range(len(F)-1):
-			c  = []
-			if F[f1] != F[f2+1] and F[f2+1] not in aux:
-				c.append(F[f1])
-				c.append(F[f2+1])
-				C.append(c)
+    Fk = []
+    for key in repeticiones:
+        if repeticiones[key]/len(T) >= minSup:
+            Fk.append(key)
 
-		for s in F:
-			pass	
-		
-		aux.append(F[f1])
-			
-	return C
-			
+    return Fk
+
+#Funcion que crea Ck, conjuntos candidatos
+def cand_gen(Fk, k): 
+    C = []
+    for i in range(len(Fk)):
+        for j in range(i+1, len(Fk)): 
+            L1 = list(Fk[i])[:k-2]
+            L2 = list(Fk[j])[:k-2]
+            L1.sort()
+            L2.sort()
+            if L1==L2: #Si el pirmer elemento de k-2 es igual
+                C.append(Fk[i] | Fk[j]) #Se hace una union de los conjuntos
+    return C
+
 
 def apriori(T, minsup):
 	C = []
 	F = []
 	C.append(init_pass(T))
 	F.append(obtenerF(T, C[0], minsup))
-	k=1
-	#for x in F[k-1]:
-	#	print(F[0])
-	C.append(cand_gen(F[k-1], T))
-	#F.append(obtenerF(T, C[1], minsup))
-	#	k=k+1
-	print(C)
-s = [['rop', 'pol', 'lec'], ['car', 'que'], ['que', 'bot'], 
+	k = 2
+	while (len(F[k-2]) > 0):
+	 	C.append(cand_gen(F[k-2], k))
+	 	F.append(obtenerF(T, C[k-1], minsup))
+	 	k += 1
+
+
+T = [['car', 'pol', 'lec'], ['car', 'que'], ['que', 'bot'], 
 ['car', 'pol', 'que'], ['car', 'pol', 'rop', 'que', 'lec'], 
 ['pol', 'rop', 'lec'], ['pol', 'lec', 'rop']]
 
-apriori(s, 0.30)
+apriori(T, 0.30)
